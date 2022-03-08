c6c8e1f2db5eb44c838457560a17a92d3ce14f2b
Actoriu <51398008+Actoriu@users.noreply.github.com>
2022-03-09 05:53:06 +0800

    Update zsh.nix

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -7,8 +7,8 @@
       dotDir = ".config/zsh";
       history = {
         extended = true;
-        save = 100000;
-        size = 100000;
+        # save = 100000;
+        # size = 100000;
         path = "${config.xdg.dataHome}/zsh/zsh_history";
       };
 

5fda914261c4cb3062abe8920f6b313c89c65d2e
Actoriu <51398008+Actoriu@users.noreply.github.com>
2022-03-08 23:06:37 +0800

    Create fzf.nix

---
+++ modules/programs/fzf.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+  programs = {
+    fzf = {
+      enable = true;
+      # defaultCommand = "${pkgs.ripgrep}/bin/rg --files --hidden --follow --no-ignore -g '!{.git,target,node_modules,*.swp,dist,*.coffee}/*' 2> /dev/null";
+    };
+  };
+}

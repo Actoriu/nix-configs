ecf65fb46e03e64d926fbb06a1dab51c3852fcac
Actoriu <51398008+Actoriu@users.noreply.github.com>
2022-03-08 22:59:49 +0800

    Create default.nix

---
+++ modules/programs/default.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+  imports = [
+    ./bat.nix
+    ./bash.nix
+    ./direnv.nix
+    ./emacs.nix
+    ./fzf.nix
+    ./git.nix
+    # ./home.nix
+    ./neovim.nix
+    ./nix.nix
+    ./zoxide.nix
+    ./zsh.nix
+  ];
+}

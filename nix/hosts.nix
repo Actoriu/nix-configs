fefef87aed81bf4c8181e4027343a54de5ad2253
Actoriu <864149939@qq.com>
2022-04-02 17:46:40 +0800

    update hosts.nix

--- nix/hosts.nix
+++ nix/hosts.nix
@@ -1,8 +1,8 @@
 {
-  # d630 = {
-  #   localSystem = "x86_64-linux";
-  #   address = "desktop";
-  # };
+  d630 = {
+    localSystem = "x86_64-linux";
+    address = "desktop";
+  };
   # oneplus5 = {
   #   localSystem = "aarch64-linux";
   #   address = "laptop";
